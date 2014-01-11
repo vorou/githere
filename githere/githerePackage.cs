539d6ec3761fe45e43756dd7707f0149d3731089
vorou <vorou@skbkontur.ru>
2014-01-12 01:25:26 +0600

    clean up package, write to output

--- githere/githerePackage.cs
+++ githere/githerePackage.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.ComponentModel.Design;
-using System.Diagnostics;
 using System.Globalization;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio;
@@ -9,88 +8,49 @@ using Microsoft.VisualStudio.Shell.Interop;
 
 namespace vorou.githere
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the information needed to show this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(GuidList.guidgitherePkgString)]
     public sealed class GitherePackage : Package
     {
-        /// <summary>
-        /// Default constructor of the package.
-        /// Inside this method you can place any initialization code that does not require 
-        /// any Visual Studio service because at this point the package object is created but 
-        /// not sited yet inside Visual Studio environment. The place to do all the other 
-        /// initialization is the Initialize method.
-        /// </summary>
-        public GitherePackage()
-        {
-            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this.ToString()));
-        }
-
-        #region Package Members
-
-        /// <summary>
-        /// Initialization of the package; this method is called right after the package is sited, so this is the place
-        /// where you can put all the initialization code that rely on services provided by VisualStudio.
-        /// </summary>
         protected override void Initialize()
         {
-            Debug.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this.ToString()));
             base.Initialize();
 
-            // Add our command handlers for menu (commands must exist in the .vsct file)
             var mcs = GetService(typeof (IMenuCommandService)) as OleMenuCommandService;
             if (null != mcs)
             {
-                // Create the command for the menu item.
-                var menuCommandID = new CommandID(GuidList.guidgithereCmdSet, (int) PkgCmdIDList.cmdidGithere);
-                var menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+                var menuCommandId = new CommandID(GuidList.guidgithereCmdSet, (int) PkgCmdIDList.cmdidGithere);
+                var menuItem = new MenuCommand(MenuItemCallback, menuCommandId);
                 mcs.AddCommand(menuItem);
             }
         }
 
-        #endregion
-
-        /////////////////////////////////////////////////////////////////////////////
-        // Overridden Package Implementation
-
-        /// <summary>
-        /// This function is the callback used to execute a command when the a menu item is clicked.
-        /// See the Initialize method to see how the menu item is associated to this function using
-        /// the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
         private void MenuItemCallback(object sender, EventArgs e)
         {
-            // Show a Message Box to prove we were here
+            var outputWindow = GetGlobalService(typeof (SVsOutputWindow)) as IVsOutputWindow;
+
+            var guidGeneral = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+            IVsOutputWindowPane pane;
+            outputWindow.CreatePane(guidGeneral, "General", 1, 0);
+            outputWindow.GetPane(guidGeneral, out pane);
+            pane.Activate();
+            pane.OutputString("Pandy!");
+
             var uiShell = (IVsUIShell) GetService(typeof (SVsUIShell));
             var clsid = Guid.Empty;
             int result;
             ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(0,
                                                                ref clsid,
                                                                "githere",
-                                                               string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
+                                                               string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this),
                                                                string.Empty,
                                                                0,
                                                                OLEMSGBUTTON.OLEMSGBUTTON_OK,
                                                                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
                                                                OLEMSGICON.OLEMSGICON_INFO,
                                                                0,
-                                                               // false
                                                                out result));
         }
     }
