1a92245f4683757943c48095ed439e91b9cf443d
vorou <vorou@skbkontur.ru>
2014-01-12 01:16:07 +0600

    init

---
+++ githere/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace vorou.githere
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidGithere =        0x100;
+
+
+    };
+}
