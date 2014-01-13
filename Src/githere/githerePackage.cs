ca7956d308fc057c3ece19ad794f7d8c8236e51d
vorou <vorou@skbkontur.ru>
2014-01-13 20:49:29 +0600

    use dte to retrieve sln dir

--- Src/githere/githerePackage.cs
+++ Src/githere/githerePackage.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.Design;
 using System.IO;
 using System.Runtime.InteropServices;
+using EnvDTE;
 using LibGit2Sharp;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
@@ -20,13 +21,10 @@ namespace vorou.githere
             AddMenuItem();
         }
 
-        private string GetRepoDir()
+        private static string GetRepoDir()
         {
-            var slnService = GetService(typeof (IVsSolution)) as IVsSolution;
-            string slnDir;
-            string slnFile;
-            string slnOptions;
-            slnService.GetSolutionInfo(out slnDir, out slnFile, out slnOptions);
+            var dte = GetGlobalService(typeof (SDTE)) as DTE;
+            var slnDir = Path.GetDirectoryName(dte.Solution.FullName);
             return new DirectoryInfo(slnDir).Parent.FullName;
         }
 
