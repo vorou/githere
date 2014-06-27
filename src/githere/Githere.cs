ecf4779a887f51be27ba97631309bda64c14bc2a
vorou <voropaev.viktor@gmail.com>
2014-06-28 00:51:44 +0600

    and once again

--- src/githere/Githere.cs
+++ src/githere/Githere.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using Microsoft.VisualStudio.Text.Editor;
@@ -9,31 +8,45 @@ namespace githere
     /// <summary>
     /// A class detailing the margin's visual definition including both size and content.
     /// </summary>
-    internal class Githere : Canvas, IWpfTextViewMargin
+    class githere : Canvas, IWpfTextViewMargin
     {
         public const string MarginName = "githere";
-        private bool _isDisposed;
+        private IWpfTextView _textView;
+        private bool _isDisposed = false;
 
         /// <summary>
-        /// Creates a <see cref="Githere"/> for a given <see cref="IWpfTextView"/>.
+        /// Creates a <see cref="githere"/> for a given <see cref="IWpfTextView"/>.
         /// </summary>
         /// <param name="textView">The <see cref="IWpfTextView"/> to attach the margin to.</param>
-        public Githere(IWpfTextView textView)
+        public githere(IWpfTextView textView)
         {
-            Height = 20;
-            ClipToBounds = true;
-            Background = new SolidColorBrush(Colors.LightGreen);
+            _textView = textView;
+
+            this.Height = 20;
+            this.ClipToBounds = true;
+            this.Background = new SolidColorBrush(Colors.LightGreen);
 
             // Add a green colored label that says "Hello World!"
-            var label = new Label {Background = new SolidColorBrush(Colors.LightGreen), Content = "Hello World!"};
-            Children.Add(label);
+            Label label = new Label();
+            label.Background = new SolidColorBrush(Colors.LightGreen);
+            label.Content = "Hello World!";
+            this.Children.Add(label);
+
         }
+
+        private void ThrowIfDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(MarginName);
+        }
+
+        #region IWpfTextViewMargin Members
 
         /// <summary>
         /// The <see cref="Sytem.Windows.FrameworkElement"/> that implements the visual representation
         /// of the margin.
         /// </summary>
-        public FrameworkElement VisualElement
+        public System.Windows.FrameworkElement VisualElement
         {
             // Since this margin implements Canvas, this is the object which renders
             // the margin.
@@ -44,6 +57,10 @@ namespace githere
             }
         }
 
+        #endregion
+
+        #region ITextViewMargin Members
+
         public double MarginSize
         {
             // Since this is a horizontal margin, its width will be bound to the width of the text view.
@@ -51,7 +68,7 @@ namespace githere
             get
             {
                 ThrowIfDisposed();
-                return ActualHeight;
+                return this.ActualHeight;
             }
         }
 
@@ -69,10 +86,10 @@ namespace githere
         /// Returns an instance of the margin if this is the margin that has been requested.
         /// </summary>
         /// <param name="marginName">The name of the margin requested</param>
-        /// <returns>An instance of Githere or null</returns>
+        /// <returns>An instance of githere or null</returns>
         public ITextViewMargin GetTextViewMargin(string marginName)
         {
-            return (marginName == MarginName) ? this : null;
+            return (marginName == githere.MarginName) ? (IWpfTextViewMargin)this : null;
         }
 
         public void Dispose()
@@ -83,11 +100,6 @@ namespace githere
                 _isDisposed = true;
             }
         }
-
-        private void ThrowIfDisposed()
-        {
-            if (_isDisposed)
-                throw new ObjectDisposedException(MarginName);
-        }
+        #endregion
     }
 }
--- src/githere/githereFactory.cs
+++ src/githere/githereFactory.cs
@@ -4,12 +4,13 @@ using Microsoft.VisualStudio.Utilities;
 
 namespace githere
 {
+    #region githere Factory
     /// <summary>
     /// Export a <see cref="IWpfTextViewMarginProvider"/>, which returns an instance of the margin for the editor
     /// to use.
     /// </summary>
-    [Export(typeof (IWpfTextViewMarginProvider))]
-    [Name(Githere.MarginName)]
+    [Export(typeof(IWpfTextViewMarginProvider))]
+    [Name(githere.MarginName)]
     [Order(After = PredefinedMarginNames.HorizontalScrollBar)] //Ensure that the margin occurs below the horizontal scrollbar
     [MarginContainer(PredefinedMarginNames.Bottom)] //Set the container to the bottom of the editor window
     [ContentType("text")] //Show this margin for all text-based types
@@ -18,7 +19,8 @@ namespace githere
     {
         public IWpfTextViewMargin CreateMargin(IWpfTextViewHost textViewHost, IWpfTextViewMargin containerMargin)
         {
-            return new Githere(textViewHost.TextView);
+            return new githere(textViewHost.TextView);
         }
     }
+    #endregion
 }
