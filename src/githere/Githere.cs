e22f4c0de33eabf6cf2faced4e56132fee2f08c9
vorou <voropaev.viktor@gmail.com>
2014-06-27 23:53:29 +0600

    starting from scratch

---
+++ src/githere/Githere.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace githere
+{
+    /// <summary>
+    /// A class detailing the margin's visual definition including both size and content.
+    /// </summary>
+    internal class Githere : Canvas, IWpfTextViewMargin
+    {
+        public const string MarginName = "githere";
+        private bool _isDisposed;
+
+        /// <summary>
+        /// Creates a <see cref="Githere"/> for a given <see cref="IWpfTextView"/>.
+        /// </summary>
+        /// <param name="textView">The <see cref="IWpfTextView"/> to attach the margin to.</param>
+        public Githere(IWpfTextView textView)
+        {
+            Height = 20;
+            ClipToBounds = true;
+            Background = new SolidColorBrush(Colors.LightGreen);
+
+            // Add a green colored label that says "Hello World!"
+            var label = new Label {Background = new SolidColorBrush(Colors.LightGreen), Content = "Hello World!"};
+            Children.Add(label);
+        }
+
+        /// <summary>
+        /// The <see cref="Sytem.Windows.FrameworkElement"/> that implements the visual representation
+        /// of the margin.
+        /// </summary>
+        public FrameworkElement VisualElement
+        {
+            // Since this margin implements Canvas, this is the object which renders
+            // the margin.
+            get
+            {
+                ThrowIfDisposed();
+                return this;
+            }
+        }
+
+        public double MarginSize
+        {
+            // Since this is a horizontal margin, its width will be bound to the width of the text view.
+            // Therefore, its size is its height.
+            get
+            {
+                ThrowIfDisposed();
+                return ActualHeight;
+            }
+        }
+
+        public bool Enabled
+        {
+            // The margin should always be enabled
+            get
+            {
+                ThrowIfDisposed();
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// Returns an instance of the margin if this is the margin that has been requested.
+        /// </summary>
+        /// <param name="marginName">The name of the margin requested</param>
+        /// <returns>An instance of Githere or null</returns>
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            return (marginName == MarginName) ? this : null;
+        }
+
+        public void Dispose()
+        {
+            if (!_isDisposed)
+            {
+                GC.SuppressFinalize(this);
+                _isDisposed = true;
+            }
+        }
+
+        private void ThrowIfDisposed()
+        {
+            if (_isDisposed)
+                throw new ObjectDisposedException(MarginName);
+        }
+    }
+}
---
+++ src/githere/githereFactory.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace githere
+{
+    /// <summary>
+    /// Export a <see cref="IWpfTextViewMarginProvider"/>, which returns an instance of the margin for the editor
+    /// to use.
+    /// </summary>
+    [Export(typeof (IWpfTextViewMarginProvider))]
+    [Name(Githere.MarginName)]
+    [Order(After = PredefinedMarginNames.HorizontalScrollBar)] //Ensure that the margin occurs below the horizontal scrollbar
+    [MarginContainer(PredefinedMarginNames.Bottom)] //Set the container to the bottom of the editor window
+    [ContentType("text")] //Show this margin for all text-based types
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    internal sealed class MarginFactory : IWpfTextViewMarginProvider
+    {
+        public IWpfTextViewMargin CreateMargin(IWpfTextViewHost textViewHost, IWpfTextViewMargin containerMargin)
+        {
+            return new Githere(textViewHost.TextView);
+        }
+    }
+}
